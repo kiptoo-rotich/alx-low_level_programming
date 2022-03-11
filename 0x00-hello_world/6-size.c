13dd9c6989df3b90827bddded987e1ecdf143068
kiptoo-rotich <rotichenoch27@gmail.com>
2022-03-11 01:13:54 -0800

    Correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,11 @@ int main(void)
 	int intType=4;
 	char charType=1;
 	int longInt=8;
-	float floatType=4;
 
 	printf("Size of a char:%d byte(s)\n",charType);
 	printf("Size of an int:%d byte(s)\n",intType);
 	printf("Size of a long int:%d byte(s)\n",intType);
 	printf("Size of a long long int:%d byte(s)\n",longInt);
-	printf("Size of a float:%f byte(s)\n",floatType);
+	printf("Size of a float:%d byte(s)\n",intType);
 	return (0);
 }
