b3096dedaf620f7573cf29b2c03d92302c5da7de
kiptoo-rotich <rotichenoch27@gmail.com>
2022-03-11 01:05:41 -0800

    Float

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * return: Always 0(Success)
+ */
+int main(void)
+{
+	int intType=4;
+	char charType=1;
+	int longInt=8;
+	float floatType=4;
+
+	printf("Size of a char:%d byte(s)\n",charType);
+	printf("Size of an int:%d byte(s)\n",intType);
+	printf("Size of a long int:%d byte(s)\n",intType);
+	printf("Size of a long long int:%d byte(s)\n",longInt);
+	printf("Size of a float:%f byte(s)\n",floatType);
+	return (0);
+}
