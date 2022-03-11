af9b72b79f80726eab2ecad105c62c61d5e597f0
kiptoo-rotich <rotichenoch27@gmail.com>
2022-03-11 11:54:51 -0800

    Betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+ * Main -Entry point
+ *
+ * Return: Always return 0()Success
+ *
+ */
 int main(void)
 {
 	int n;
