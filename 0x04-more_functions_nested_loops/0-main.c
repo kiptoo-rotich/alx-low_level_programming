1bdbf99eec643f6cee36526f3d9d90855c3f4df9
kiptoo-rotich <rotichenoch27@gmail.com>
2022-03-17 13:43:29 -0400

    Hot fix

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,18 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include<ctype.h>
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, isupper(c));
-    return (0);
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
 }
